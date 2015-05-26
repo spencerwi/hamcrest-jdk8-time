f535133edfefb6f40d4d57e774514b82527d3db7
Zakhi Verech <zakhiv@gmail.com>
2015-05-26 22:24:39 +0300

    Add tests for IsWithin
    Add tests for not matching

--- src/test/java/com/spencerwi/hamcrestJDK8Time/matchers/IsWithinLocalDateTimeTest.java
+++ src/test/java/com/spencerwi/hamcrestJDK8Time/matchers/IsWithinLocalDateTimeTest.java
@@ -6,10 +6,14 @@ import java.time.LocalDateTime;
 
 import static com.spencerwi.hamcrestJDK8Time.matchers.IsWithin.within;
 import static java.time.temporal.ChronoUnit.DAYS;
+import static java.time.temporal.ChronoUnit.HOURS;
 import static java.time.temporal.ChronoUnit.MINUTES;
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.fail;
+
 
 public class IsWithinLocalDateTimeTest {
     
@@ -29,12 +33,28 @@ public class IsWithinLocalDateTimeTest {
     }
 
     @Test
+    public void doesNotMatchesWhenGivenDateIsNotWithinWindow(){
+        LocalDateTime now = LocalDateTime.now(),
+                yesterday = LocalDateTime.now().minusDays(1),
+                 tomorrow = LocalDateTime.now().plusDays(1),
+            sixMinutesAgo = LocalDateTime.now().minusMinutes(6),
+        sixMinutesFromNow = LocalDateTime.now().plusMinutes(6);
+
+        assertThat(yesterday, is(not(within(20, HOURS).of(now))));
+        assertThat(tomorrow,  is(not(within(20, HOURS).of(now))));
+
+        assertThat(sixMinutesAgo,     is(not(within(5, MINUTES).of(now))));
+        assertThat(sixMinutesFromNow, is(not(within(5, MINUTES).of(now))));
+    }
+
+    @Test
     public void describesItselfCorrectly(){
         LocalDateTime now = LocalDateTime.now();
         LocalDateTime lastWeek = LocalDateTime.now().minusWeeks(1l);
 
         try {
             assertThat(lastWeek, is(within(1, DAYS).of(now))); /* false assertion to trigger AssertionError */
+            fail();
         } catch(AssertionError e){
             assertThat(e.getMessage(), containsString("a LocalDateTime that is within <1L> <" + DAYS.toString() + "> of <" + now.toString() + ">"));
         }
--- src/test/java/com/spencerwi/hamcrestJDK8Time/matchers/IsWithinZonedDateTimeTest.java
+++ src/test/java/com/spencerwi/hamcrestJDK8Time/matchers/IsWithinZonedDateTimeTest.java
@@ -5,11 +5,13 @@ import org.junit.Test;
 import java.time.ZonedDateTime;
 
 import static com.spencerwi.hamcrestJDK8Time.matchers.IsWithin.within;
-import static java.time.temporal.ChronoUnit.DAYS;
-import static java.time.temporal.ChronoUnit.MINUTES;
+import static java.time.temporal.ChronoUnit.*;
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.fail;
+
 
 public class IsWithinZonedDateTimeTest {
     
@@ -29,12 +31,28 @@ public class IsWithinZonedDateTimeTest {
     }
 
     @Test
+    public void doesNotMatchWhenGivenDateIsNotWithinWindow(){
+        ZonedDateTime now = ZonedDateTime.now(),
+                yesterday = ZonedDateTime.now().minusDays(1),
+                 tomorrow = ZonedDateTime.now().plusDays(1),
+            sixMinutesAgo = ZonedDateTime.now().minusMinutes(6),
+        sixMinutesFromNow = ZonedDateTime.now().plusMinutes(6);
+
+        assertThat(yesterday, is(not(within(20, HOURS).of(now))));
+        assertThat(tomorrow,  is(not(within(20, HOURS).of(now))));
+
+        assertThat(sixMinutesAgo,     is(not(within(5, MINUTES).of(now))));
+        assertThat(sixMinutesFromNow, is(not(within(5, MINUTES).of(now))));
+    }
+
+    @Test
     public void describesItselfCorrectly(){
         ZonedDateTime now = ZonedDateTime.now();
         ZonedDateTime lastWeek = ZonedDateTime.now().minusWeeks(1l);
 
         try {
             assertThat(lastWeek, is(within(1, DAYS).of(now))); /* false assertion to trigger AssertionError */
+            fail();
         } catch(AssertionError e){
             assertThat(e.getMessage(), containsString("a ZonedDateTime that is within <1L> <" + DAYS.toString() + "> of <" + now.toString() + ">"));
         }
